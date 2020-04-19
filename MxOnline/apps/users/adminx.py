3d08962f3bd51bc763e66972e7b56968915b0182
浮生未歇 <764420453@qq.com>
2020-04-19 15:25:33 +0800

    xadmin后台全局配置

--- MxOnline/apps/users/adminx.py
+++ MxOnline/apps/users/adminx.py
@@ -1,5 +1,22 @@
 import xadmin
 from apps.users.models import EmailVerifyRecord, Banner
+from xadmin import views
+
+
+
+class BaseSetting(object):
+    enable_themes = True  # 开启主题功能
+    use_bootswatch = True
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+
+class GlobalSettings(object):
+    site_title = "慕学后台管理系统"
+    site_footer = "慕学在线网"
+    menu_style = "accordion"
+xadmin.site.register(views.CommAdminView, GlobalSettings)
+
+
 
 
 class EmailVerifyRecordAdmin(object):
