d18c34554efe1e83a60519f8e65b92977a4bdb91
浮生未歇 <764420453@qq.com>
2020-04-19 14:22:26 +0800

    后台组织的添加

---
+++ MxOnline/apps/organization/adminx.py
@@ -0,0 +1,29 @@
+import xadmin
+from apps.organization.models import Teacher, CityDict, CourseOrg
+
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums']
+
+
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org', 'name', 'work_years', 'work_company']
+
+
+xadmin.site.register(Teacher, TeacherAdmin)
